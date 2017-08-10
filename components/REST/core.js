266910becde6914acbc0b7cde508f1a9fde230c8
boveloco <boveloco@gmail.com>
2017-08-10 12:17:29 -0300

    Better Documentation GenerateOptionsMethod to help with options object

--- components/REST/core.js
+++ components/REST/core.js
@@ -1,21 +1,52 @@
 exports = module.exports = function(request) {
 	/**
-	 * options {
-	 * method,
-	 * url,
-	 * headers,
-	 * }
+	 * Process a request given an options object
+	 * @param {Object} options Options object for the request. (See generateOptions)
+	 * @param {Function} cb callback to be called when processed the request.
 	 */
 	var basicRest = (options, cb) => {
 		request(options, (err, response, body) => {
 			if (err) {
 				return cb(err);
 			}
-			cb({ status: response.status, body: body });
+			cb(null, { status: response.statusCode, body: body });
 		});
-	}
+	};
 
-	return basicRest;
+	/**
+	 * Generates the options to REQUEST
+	 * @param {String} url the url to be called
+	 * @param {String} method method: PUT, POST, DELETE, GET
+	 * @param {Object} headers Headers to HTTP Request
+	 * @param {Object} obj The body obj, if there is
+	 * @return {Objcet} Options object returned
+	 *
+	 * Return Object
+	 * {	url: String,
+	 * 		method: String,
+	 * 		headers: Object,
+	 * 		obj: Object
+	 * }
+	 *
+	 * Headers Object
+	 * {	'User-Agent': 'request',
+	 * 		'Content-Type': 'text/html'
+	 * }
+	 */
+	var generateOptions = (url, method, headers, obj) => {
+		if (!url) { return new Error('No url found') }
+		if (!method) { return new Error('No method found') }
+		var ret = { url: url, method: method };
+		if (headers) { ret.headers = headers }
+		if (obj) { ret.body = obj }
+
+		return ret;
+	};
+
+	return {
+		request: basicRest,
+		createOptions: generateOptions
+	};
 }
 exports['@singleton'] = true;
 exports['@require'] = ['request'];
