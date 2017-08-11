ee3e741752c6511c0fdeb29c28aef046e246ec53
boveloco <boveloco@gmail.com>
2017-08-11 14:12:54 -0300

    Tests

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ exports = module.exports = function(service) {
 		create: function() {
 			service.initiate('http://tcc-andre.ddns.net/queue', 'minhavidaeandarporestepais');
 			service.createQueue('teste', (err, id) => {
-				})
-			service.addPlayer('123456123', { name: 'andre', city: 'srs' }, (err, id) => {
-				console.log(err, id);
+				service.addPlayer(id, { name: 'andre', city: 'srs' }, (err, idP) => {
+					if (err) { console.log(err) }
+					service.removePlayer(id, idP, (err, resp) => {
+						if (err) { return console.log(err) }
+						console.log(resp);
+					})
+				});
+				service.removeQueue(id, (err, resp) => {
+					console.log(resp);
+				});
 			});
+		},
+		getAll: function() {
+			service.getAllQueues((err, resp) => {
+				if (err) {
+					return console.log(err);
+				}
+				console.log(resp);
+			})
 		}
 	}
-
 };
 
 exports['@singleton'] = true;
