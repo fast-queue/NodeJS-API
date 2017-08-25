5b908f5f7c424f8549e64ff2b5633464751d0363
boveloco <boveloco@gmail.com>
2017-08-25 19:45:04 -0300

    component version some package info

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var IoC = require('electrolyte');
-
-IoC.use(IoC.node_modules());
-IoC.use(IoC.dir('components'));
-IoC.use(IoC.dir('.'));
-
-return IoC.create('app');
+exports.run = function(){
+    var IoC = require('electrolyte');
+    
+    IoC.use(IoC.node_modules());
+    IoC.use(IoC.dir('components'));
+    IoC.use(IoC.dir('.'));
+    
+    return IoC.create('app');
+}
